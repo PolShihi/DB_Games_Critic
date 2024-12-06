2f9745a006243436c543beb793e778f5dd600fc9
PolShihi <zhora.lyamtsev@gmail.com>
2024-12-06 11:33:59 +0300

    Add project files.

---
+++ Games_critic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Games_critic.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
